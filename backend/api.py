064224e20626df9321f505a4d7de4238b29dbc78
kritikarastogi-codes <kritikarastogi210906@gmail.com>
2026-08-08 15:59:21 +0530

    Configure CORS for frontend

--- backend/api.py
+++ backend/api.py
@@ -10,12 +10,13 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
     ],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
+allow_origins=["*"]
 
 
 class Complaint(BaseModel):
